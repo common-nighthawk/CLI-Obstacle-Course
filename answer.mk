fff4a0483a701673942550fe33d900a8650766c9
common-nighthawk <danielallendeutsch@gmail.com>
2014-02-28 14:51:22 -0500

    Create answer.mk

---
+++ answer.mk
@@ -0,0 +1,177 @@
+  681  mkdir 'CLI-Obstacle-Course'
+  682  ls
+  683  cd 'CLI-Obstacle-Course'
+  684  pwd
+  685  git clone https://github.com/dbc-challenges/CLI-Obstacle-Course
+  686  pwd
+  687  ls
+  688  cd 'CLI-Obstacle-Course'
+  689  pwd
+  690  ls
+  691  history
+  692  pwd
+  693  ls -d
+  694  ls
+  695  ls -a
+  696  ls -l
+  697  ls -l -a
+  698  ls -la
+  699  mv images app/assets
+  700  ls -la
+  701  cd app
+  702  ls
+  703  cd assets
+  704  ls
+  705  pwd
+  706  cd ..
+  707  pw
+  708  pwd
+  709  ls
+  710  cd views
+  711  ls
+  712  cd static_pages
+  713  pwd
+  714  ls
+  715  ls ..
+  716  ls .. ..
+  717  pwd
+  718  ls
+  719  cd ..
+  720  pwd
+  721  ls
+  722  pwd
+  723  cd ..
+  724  pwd
+  725  ls
+  726  cd assets
+  727  pwd
+  728  ls
+  729  cd stylesheets
+  730  pwd
+  731  ls
+  732  pwd
+  733  ls
+  734  ls ..
+  735  ls ...
+  736  ls .. ..
+  737  ls ../
+  738  ls
+  739  ls
+  740  ls ../
+  741  ls ../../
+  742  ls ../../../
+  743  ls ../../../../
+  744  ls ../../../../../
+  745  ls ../../../../../../
+  746  ls ../../../../../../../
+  747  ls
+  748  pwd
+  749  cd ..
+  750  ls
+  751  pwd
+  752  cd ..
+  753  ls
+  754  cd views
+  755  ls
+  756  cd static_pages
+  757  ls
+  758  ls ../
+  759  pwd
+  760  ls
+  761  mv this_is_the_annoying_ping_pong.text\!.txt ../
+  762  ls
+  763  mv this_is_the_annoying_ping_pong.text\!.txt ~/../
+  764  mv this_is_the_annoying_ping_pong.text\!.txt ~/views
+  765  mv this_is_the_annoying_ping_pong.text\!.txt ../
+  766  ls
+  767  mv this_is_the_annoying_ping_pong.text\!.txt ..
+  768  mv this_is_the_annoying_ping_pong.text\!.txt ..
+  769  ls
+  770  mv this_is_the_annoying_ping_pong.text\!.txt where_am_you\?.txt
+  771  ls
+  772  mv this_is_the_annoying_ping_pong_text\!.txt ../
+  773  ls
+  774  ls /../
+  775  ls
+  776  cd ..
+  777  ls
+  778  cd static_pages
+  779  ls
+  780  ls ..
+  781  mv /../this_is_the_annoying_ping_pong_text\!.txt .
+  782  mv /../this_is_the_annoying_ping_pong_text\!.txt .
+  783  mv ../this_is_the_annoying_ping_pong_text\!.txt .
+  784  ls
+  785  ls ..
+  786  mv this_is_the_annoying_ping_pong_text\!.txt where_am_you\?.txt
+  787  ls
+  788  mv where_am_you?.txt where_am_i\?.txt
+  789  ls
+  790  cat where_am_i?.txt
+  791  ech pwd >where_am_i?.txt
+  792  echo pwd >where_am_i?.txt
+  793  cat where_am_i?.txt
+  794  pwd
+  795  echo /Users/Daniel/Documents/Dev Bootcamp/Phase 0/Unit 3/CLI-Obstacle-Course/CLI-Obstacle-Course/app/views/static_pages >where_am_i?.txt
+  796  cat where_am_i?.txt
+  797  cat where_am_i?.txt
+  798  cd ../../../../../..
+  799  pwd
+  800  ls
+  801  cd 'Unit 3'
+  802  ls
+  803  cd CLI-Obstacle_course
+  804  cd CLI-Obstacle-Course
+  805  ls
+  806  cd CLI-Obstacle-Course
+  807  ls
+  808  find static_pages_controller.rb
+  809  find -static_pages_controller.rb
+  810  find *static_pages_controller.rb
+  811  find -name '*static_pages_controller.rb'
+  812  find '*.static_pages_controller.rb'
+  813  find *.static_pages_controller.rb
+  814  find ~/ -name '*.rb'
+  815  find -name '*.rb'
+  816  find ~/./ -name '*.rb'
+  817  find ~/./ -name '*static_pages_controller.rb'
+  818  pwd
+  819  mv /app/helpers/static_pages_controller.rb
+  820  mv /app/helpers/static_pages_controller.rb /app/controllers
+  821  mv /./app/helpers/static_pages_controller.rb /./app/controllers
+  822  ls
+  823  pwd
+  824  mv app/helpers/static_pages_controller.rb app/controllers
+  825  cd app
+  826  cd controller
+  827  cd controllers
+  828  ls
+  829  cd ../../
+  830  pwd
+  831  find -name '*static_pages_controller.rb'
+  832  find ~/./ -name '*controller*'
+  833  grep "bootinator"
+  834  grep "bootinator" .
+  835  grep "bootinator" ./
+  836  grep "bootinator" /./*
+  837  ls
+  838  grep "bootinator" /./*
+  839  grep -r "bootinator"
+  840  grep -r bootinator
+  841  pwd
+  842  grep -nr bootinator /Users/Daniel/Documents/Dev Bootcamp/Phase 0/Unit 3/CLI-Obstacle-Course/CLI-Obstacle-Course
+  843  grep -nr bootinator
+  844  grep -nr 'bootinator' .
+  845  mv app/assets/stylesheets/application_helper.rb app/helpers
+  846  ls
+  847  rm delete_me
+  848  ls
+  849  rmdir delete_me
+  850  rm -rf delete_me
+  851  ls
+  852  find . -name '*winning.rb'
+  853  ls -a
+  854  pwd
+  855  mv config/environments/you_are_winning.rb .
+  856  ls
+  857  ruby you_are_winning.rb
